95d10ee907ac83237c29b8e9f23d1e1beba69f72
wileyj <wileyj@users.noreply.github.com>
2020-10-21 18:11:40 -0400

    adding a bootstrap service to compose/setup cleanup
    removing the npx portion; we'll use a key defined here:
    https://github.com/blockstack/stacks-blockchain-api/blob/e6e445b2cd1055f4bf25af7af646405783d5877e/src/api/routes/debug.ts#L36-L52

--- setup.sh
+++ setup.sh
@@ -1,44 +1,38 @@
 #/bin/sh
+FOLLOWER_TOML="./stacks-node-follower/Config.toml"
+MINER_TOML="./stacks-node-miner/Config.toml"
 echo ""
-echo  "Generating keychain as file: '.stx_keychain'"
-npx blockstack-cli@1.1.0-beta.1 make_keychain -t > .stx_keychain 2>/dev/null
-echo  "Setting local vars from .env file"
-export $(grep -v '^#' .env | xargs)
-echo  "Retrieving private seed from keychain"
-if [ ! -f .stx_keychain ]; then
+if [ ! -f .env ]; then
   echo ""
   echo "*********************************"
   echo "Error:"
-  echo "  File .stx_keychain wasn't created"
-  echo "    Manually Run: npx blockstack-cli@1.1.0-beta.1 make_keychain -t > .stx_keychain"
-  echo "    And note any error output"
-  exit 1
-fi
-export PRIVATE_KEY=$(cat .stx_keychain | jq .keyInfo.privateKey | tr -d '"')
-# PUBLIC_KEY=$(cat .stx_keychain | jq .keyInfo.publicKey | tr -d '"')
-
-if [ ! $PRIVATE_KEY ]; then
+  echo "  File .env is missing"
+  echo "  Try 'git pull' or 'git checkout .env'"
   echo ""
-  echo "*********************************"
-  echo "Error:"
-  echo "  Private Key missing from .stx_keychain"
-  echo "    Likely there were errors creating the file."
-  echo "    Manually Run: npx blockstack-cli@1.1.0-beta.1 make_keychain -t"
-  echo "    And note any error output"
   exit 2
 fi
+echo  "Setting local vars from .env file"
+export $(grep -v '^#' .env | xargs)
 
-echo "Updating configs with values from files: .env, .stx_keychain"
-envsubst "`env | awk -F = '{printf \" $%s\", $1}'`" \
-  < stacks-node-follower/Config.toml.template \
-  > stacks-node-follower/Config.toml
-envsubst "`env | awk -F = '{printf \" $%s\", $1}'`" \
-  < stacks-node-miner/Config.toml.template \
-  > stacks-node-miner/Config.toml
-
+if [ -f ${FOLLOWER_TOML}.template -a -f ${MINER_TOML}.template ];then
+  echo "Updating configs with values from files: .env, .stx_keychain"
+  envsubst "`env | awk -F = '{printf \" $%s\", $1}'`" \
+    < ${FOLLOWER_TOML}.template \
+    > ${FOLLOWER_TOML}
+  envsubst "`env | awk -F = '{printf \" $%s\", $1}'`" \
+    < ${MINER_TOML}.template \
+    > ${MINER_TOML}
+else
+  echo ""
+  echo "*********************************"
+  echo "Error: missing template file(s)"
+  echo "  Try 'git pull'"
+  echo "  or:"
+  echo "    'git checkout stacks-node-follower/Config.toml.template; git checkout stacks-node-miner/Config.toml.template'"
+  echo ""
+  exit 3
+fi
 echo  "Unsetting local vars from .env file"
-unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
-unset PRIVATE_KEY
 echo "Now, run:"
 echo "  docker-compose up -d"
 echo "  Hint: to stop everything, run 'docker-compose down'"
