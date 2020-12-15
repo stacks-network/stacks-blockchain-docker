ebe88c37b38e37275c5cf0f0b7b79dafa211111d
wileyj <wileyj@users.noreply.github.com>
2020-12-15 14:15:49 -0500

    Adding btc regtest
    - cleaning up a bit
    - comment the explorer from docker-compose
    - update stacks-node-api ENV VARS to request faucet funds

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #/bin/sh
-FOLLOWER_TOML="./stacks-node-follower/Config.toml"
-MINER_TOML="./stacks-node-miner/Config.toml"
+FOLLOWER_CONFIG="./stacks-node-follower/Config.toml"
+MINER_CONFIG="./stacks-node-miner/Config.toml"
+BTC_CONFIG="./bitcoin/bitcoin.conf"
+BTC_CONTROLLER_CONFIG="./bitcoin/Config.toml"
+export EPOCH=$(echo `date +%s`) # don't think this works for windows
 echo ""
 if [ ! -f .env ]; then
   echo ""
@@ -14,14 +17,14 @@ fi
 echo  "Setting local vars from .env file"
 export $(grep -v '^#' .env | xargs)
 
-if [ -f ${FOLLOWER_TOML}.template -a -f ${MINER_TOML}.template ];then
-  echo "Updating configs with values from files: .env, .stx_keychain"
+if [ -f ${FOLLOWER_CONFIG}.template -a -f ${MINER_CONFIG}.template ];then
+  echo "Updating Stacks Configs with values from files: .env"
   envsubst "`env | awk -F = '{printf \" $%s\", $1}'`" \
-    < ${FOLLOWER_TOML}.template \
-    > ${FOLLOWER_TOML}
+    < ${FOLLOWER_CONFIG}.template \
+    > ${FOLLOWER_CONFIG}
   envsubst "`env | awk -F = '{printf \" $%s\", $1}'`" \
-    < ${MINER_TOML}.template \
-    > ${MINER_TOML}
+    < ${MINER_CONFIG}.template \
+    > ${MINER_CONFIG}
 else
   echo ""
   echo "*********************************"
@@ -32,9 +35,31 @@ else
   echo ""
   exit 3
 fi
+
+if [ -f ${BTC_CONFIG}.template -a -f ${BTC_CONTROLLER_CONFIG}.template ];then
+  echo "Updating Bitcoin Configs with values from files: .env"
+  envsubst "`env | awk -F = '{printf \" $%s\", $1}'`" \
+    < ${BTC_CONFIG}.template \
+    > ${BTC_CONFIG}
+  envsubst "`env | awk -F = '{printf \" $%s\", $1}'`" \
+    < ${BTC_CONTROLLER_CONFIG}.template \
+    > ${BTC_CONTROLLER_CONFIG}
+else
+  echo ""
+  echo "*********************************"
+  echo "Error: missing template file(s)"
+  echo "  Try 'git pull'"
+  echo "  or:"
+  echo "    'git checkout bitcoin/Config.toml.template; git checkout bitcoin/bitcoin.conf.template'"
+  echo ""
+  exit 3
+fi
+
 echo ""
 echo "Stacks V2 Configs created:"
-echo "  - ${FOLLOWER_TOML}"
-echo "  - ${MINER_TOML}"
+echo "  - ${FOLLOWER_CONFIG}"
+echo "  - ${MINER_CONFIG}"
+echo "  - ${BTC_CONFIG}"
+echo "  - ${BTC_CONTROLLER_CONFIG}"
 echo ""
 exit 0
