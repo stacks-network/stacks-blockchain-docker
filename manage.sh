4ff86fd20fb5dc8fe1a795ddceb99c25eee03ee6
wileyj <2847772+wileyj@users.noreply.github.com>
2021-04-30 18:36:21 -0400

    first pass, fixed a few issues here and there. started to update the wrapper a bit. all networks are launching

--- manage.sh
+++ manage.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 network=$1
 action=$2
-if ( [[ $network != "mainnet" ]] && [[ $network != "testnet" ]] && [[ $network != "mocknet" ]] ) || ( [[ $action != "up" ]] && [[ $action != "down" ]] && [[ $action != "logs" ]] && [[ $action != "reset" ]] ); then
+
+usage() {
+	echo ""
 	echo "Usage: $0 [mainnet|testnet|mocknet] [up|down|logs|reset]"
-	exit
+	exit 0
+}
+
+ordered_stop() {
+	# if we're not mocknet, we have to bring down stacks-node before the api or the data is corrupt
+	echo "Stopping stacks-node-follower first to prevent database errors"
+	docker-compose -f ./configurations/common.yaml -f ./configurations/$network.yaml stop stacks-node-follower
+}
+
+if [[ $network == "bns" ]]; then
+	echo "Downloading and extracting V1 bns-data"
+	docker-compose -f ./configurations/bns.yaml up
+	docker-compose -f ./configurations/bns.yaml down
+	usage
+fi
+
+if ( [[ $network != "mainnet" ]] && [[ $network != "testnet" ]] && [[ $network != "mocknet" ]] ) || ( [[ $action != "up" ]] && [[ $action != "down" ]] && [[ $action != "logs" ]] && [[ $action != "reset" ]]); then
+	usage
 fi
 if [[ $action == "reset" ]]; then
-	rm -rf ./persistent-data/$network
+	# bring down containers
+	if [ -d ./persistent-data/$network ]; then
+		echo "Resetting Persistent data for $network"
+		rm -rf ./persistent-data/$network
+	fi
 else
 	if [[ $action == "up" ]] && [[ $(docker-compose -f configurations/common.yaml ps -q) ]]; then
 		echo "Network already running, bring it down using '$0 $network down' first."
-		exit
+		exit 1
+	fi
+	if [[ $network != "mocknet" ]];then
+		if [[ $action == "down" ]]; then
+			# we have to bring down the follower node first, else the DB can have missing data
+			ordered_stop
+			# continue to the generic "down" later in script
+		else
+			mkdir -p ./persistent-data/$network
+		fi
 	fi
 	[[ ! -f "./configurations/$network/Config.toml" ]] && cp ./configurations/$network/Config.toml.sample ./configurations/$network/Config.toml
 	[[ $action == "up" ]] && param=-d
@@ -18,3 +50,4 @@ else
 	docker-compose -f ./configurations/common.yaml -f ./configurations/$network.yaml $action $param
 	[[ $action == "up" ]] && echo "Brought up $network, use '$0 $network logs' to follow log files."
 fi
+exit 0
