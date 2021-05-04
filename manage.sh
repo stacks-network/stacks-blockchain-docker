0cd5a111491aa5a4f63ebf306719f4da48bde047
wileyj <2847772+wileyj@users.noreply.github.com>
2021-05-04 00:04:07 -0400

    adding function to prevent multiple networks
    commenting the env `BNS_IMPORT_DIR` to reduce startuptime by default.
    uncommenting this line will import v1 data

--- manage.sh
+++ manage.sh
@@ -22,6 +22,15 @@ usage() {
 	exit 0
 } 
 
+check_network() {
+	if [[ $(docker-compose -f configurations/common.yaml ps -q) ]]; then
+		# docker running
+		return 0
+	fi
+	# docker is not running
+	return 1
+}
+
 download_bns_data() {
 	echo "Downloading and extracting V1 bns-data"
 	echo "Running: docker-compose -f ./configurations/bns.yaml up"
@@ -34,7 +43,8 @@ download_bns_data() {
 
 reset_data() {
 	if [ -d ./persistent-data/${NETWORK} ]; then
-		if [[ ! $(docker-compose -f configurations/common.yaml ps -q) ]]; then
+		# if [[ ! $(docker-compose -f configurations/common.yaml ps -q) ]]; then
+		if ! check_network; then
 			echo "Resetting Persistent data for ${NETWORK}"
 			echo "Running: rm -rf ./persistent-data/${NETWORK}"
 			rm -rf ./persistent-data/${NETWORK}
@@ -55,19 +65,55 @@ ordered_stop() {
 	docker-compose -f ./configurations/common.yaml -f ./configurations/${NETWORK}.yaml stop stacks-node-follower
 }
 
+docker_logs(){
+	PARAM="-f"
+	if ! check_network; then
+		echo 
+		echo "*** No ${NETWORK} services running ***"
+		usage
+	fi
+	run_docker
+}
+
+docker_down () {
+	ACTION="down"
+	if ! check_network; then
+		echo
+		echo "*** stacks-node network is not running ***"
+		echo 
+		return
+	fi
+	if [ ${NETWORK} != "mocknet" ];then
+		ordered_stop
+	fi
+	run_docker
+}
+
+docker_up() {
+	ACTION="up"
+	if check_network; then
+		echo
+		echo "*** stacks-node network is already running ***"
+		echo
+		return
+	fi
+	[[ ! -f "./configurations/${NETWORK}/Config.toml" ]] && cp ./configurations/${NETWORK}/Config.toml.sample ./configurations/${NETWORK}/Config.toml
+	PARAM="-d"
+	run_docker
+}
+
 run_docker() {
 	echo "Running: docker-compose -f ./configurations/common.yaml -f ./configurations/${NETWORK}.yaml ${ACTION} ${PARAM}"
 	docker-compose -f ./configurations/common.yaml -f ./configurations/${NETWORK}.yaml ${ACTION} ${PARAM}
 	if [[ $? -eq 0 && ${ACTION} == "up" ]]; then
 		echo "Brought up ${NETWORK}, use '$0 ${NETWORK} logs' to follow log files."
 	fi
-	exit $?
 }
 
 
 case ${NETWORK} in
 	mainnet | testnet | mocknet)
-		if [[ ${NETWORK} != "mocknet" && ${ACTION} != "reset" ]];then
+		if [[ ${NETWORK} != "mocknet" && ${ACTION} != "reset" && ! -d ./persistent-data/${NETWORK} ]];then
 			echo "Creating persistent-data for ${NETWORK}"
 			mkdir -p ./persistent-data/${NETWORK}
 		fi
@@ -82,33 +128,28 @@ esac
 
 case ${ACTION} in 
 	up|start)
-		ACTION="up"
-		[[ ! -f "./configurations/${NETWORK}/Config.toml" ]] && cp ./configurations/${NETWORK}/Config.toml.sample ./configurations/${NETWORK}/Config.toml
-		PARAM="-d"
+		docker_up
 		;;
 	down|stop)
-		ACTION="down"
-		if [ ${NETWORK} != "mocknet" ];then
-			ordered_stop
-		fi
+		docker_down
+		;;
+	restart)
+		docker_down
+		docker_up
 		;;
 	logs)
-		PARAM="-f"
-		if [[ ! $(docker-compose -f configurations/common.yaml ps -q) ]]; then
-			echo 
-			echo "*** No ${NETWORK} services running ***"
-			usage
-		fi
+		docker_logs
 		;;
 	upgrade|pull)
 		ACTION="pull"
+		run_docker
 		;;
 	reset)
 		reset_data
+		run_docker
 		;;
 	*)
 		usage
 		;;
 esac
-run_docker
-exit
+exit $?
