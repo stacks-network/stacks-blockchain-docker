0b0f6f1878077f15d51e46c1b04f4a4c04f0f89f
wileyj <2847772+wileyj@users.noreply.github.com>
2022-05-11 10:46:22 -0700

    Deleting private-testnet and packer references
    - private-testnet no longer used
    - packer has moved to separate repo

--- packer/build_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION=$(curl -sL https://api.github.com/repos/stacks-network/stacks-blockchain/releases/latest | jq .tag_name | tr -d '"')
-echo "Setting Version to ${VERSION}"
-echo ""
-echo "Building private-testnet"
-echo "    version: ${VERSION}"
-packer build --var-file=vars.json --var "version=${VERSION}" private-testnet.json
-
